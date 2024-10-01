f07cd2e472678589698a8663988494b3e06ef174
trichuu <hong2jie2@outlook.com>
2024-10-01 23:17:54 +0800

    fix Awaiter concept

--- include/coroutine_concepts.hpp
+++ include/coroutine_concepts.hpp
@@ -1,14 +1,17 @@
 #pragma once
 #include <concepts>
+#include <coroutine>
 namespace cocos::concepts {
 template <typename A>
-concept Awaiter = requires(A a) {
+concept Awaiter = requires(A a, std::coroutine_handle<> h) {
   { a.await_ready() } -> std::convertible_to<bool>;
-  a.await_suspend();
+  a.await_suspend(h);
   a.await_resume();
 };
 template <typename A>
 concept Awaitable = Awaiter<A> || requires(A a) {
-  { a.operator co_await } -> Awaiter;
+  { a.operator co_await() } -> Awaiter;
+} || requires(A a) {
+  { operator co_await(static_cast<A&&>(a)) } -> Awaiter;
 };
 } // namespace cocos::concepts

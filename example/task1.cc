bb16a44a2e36f37017f17c6a4164610876731c06
trichuu <hong2jie2@outlook.com>
2024-10-02 03:29:29 +0800

    task1 now show the running thread

--- example/task1.cc
+++ example/task1.cc
@@ -2,22 +2,25 @@
 #include "../include/task.hpp"
 #include <format>
 #include <iostream>
-
-
+#include <thread>
 #define print(...) (std::cout << std::format(__VA_ARGS__))
 
 using namespace std::chrono_literals;
 
 cocos::Task<double> task1() {
-  print("Task 1 started\n");
+  auto start = std::chrono::steady_clock::now();
+  print("Task 1 started at thread {}.\n", std::this_thread::get_id());
   co_await cocos::sleep(1s);
-  print("Task 1 finished\n");
+  print("Task 1 finished at thread {}, slept for {}.\n",
+        std::this_thread::get_id(), std::chrono::steady_clock::now() - start);
   co_return 2.5;
 }
 cocos::Task<int> task2() {
-  print("Task 2 started\n");
+  auto start = std::chrono::steady_clock::now();
+  print("Task 2 started at thread {}.\n", std::this_thread::get_id());
   co_await cocos::sleep(2s);
-  print("Task 2 finished\n");
+  print("Task 2 finished at thread {}, slept for {}.\n",
+        std::this_thread::get_id(), std::chrono::steady_clock::now() - start);
   co_return 42;
 }
 
@@ -25,6 +28,7 @@ int main() {
   auto &loop = cocos::EventLoop::get_loop();
   auto t1 = task1();
   auto t2 = task2();
+  print("Program begins at Main thread: {}.\n", std::this_thread::get_id());
   auto now = std::chrono::steady_clock::now();
   loop.add_task(t1);
   loop.add_task(t2);

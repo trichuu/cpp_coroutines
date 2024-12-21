d676fbfb3ccd1f47b696949bff4601976ccee8de
trichuu <hong2jie2@outlook.com>
2024-12-21 21:54:03 +0800

    fix whenall logic error

--- include/whenall.hpp
+++ include/whenall.hpp
@@ -4,11 +4,12 @@
 #include "task.hpp"
 #include <coroutine>
 namespace coros {
-struct CheckAwaiter {
-  std::coroutine_handle<> handle;
-  bool await_ready() { return handle.done(); }
+template <typename T> struct WaitFinishedAwaiter {
+  Task<T> *task;
+
+  bool await_ready() { return task->raw_handle().done(); }
   std::coroutine_handle<> await_suspend(std::coroutine_handle<> h) {
-    EventLoop::get_loop().add_task(h);
+    this->task->raw_handle().prev_hdl = h;
     return std::noop_coroutine();
   }
   void await_transform() {}
@@ -18,7 +19,7 @@ template <typename Iter> Task<void> when_all(Iter begin, Iter end) {
     EventLoop::get_loop().add_task(*it);
   }
   for (auto it{begin}; it != end; ++it) {
-    co_await CheckAwaiter{(*it).raw_handle()};
+    co_await WaitFinishedAwaiter{std::addressof(*it)};
   }
 }
 } // namespace coros
